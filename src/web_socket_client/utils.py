ab31ea9501104be130db83bd680347b850470f55
unknown <Admin@M00159.akvilon-invest.local>
2025-03-28 18:18:33 +0300

    test2

--- src/web_socket_client/utils.py
+++ src/web_socket_client/utils.py
@@ -1,8 +1,25 @@
 import json
 from web_socket_client.settings import logfire
 from src.parser.task import TaskParser
+from src.email_akv.email_sender import EmailSender
+
+def generate_approvers_html(approve_data):
+    approvers_html = ""
+    
+    if "approvers" in approve_data:
+        for approver in approve_data["approvers"]:
+            approvers_html += f'<tr><td>{approver["name"]}</td><td>{approver["status"]}</td><td>{approver["approved_date"]}</td></tr>\n'
+    
+    return approvers_html
 
 
+def process_string(input_string: str, num: int) -> str:
+    if len(input_string) > num:
+        return input_string[:num] + "..."
+    else:
+        return input_string
+
+tehzak_email = EmailSender()
 
 async def custom_handle_message(data):
     """
@@ -45,10 +62,47 @@ async def custom_handle_message(data):
         approval_text = data.get('reply').get('text')
         logfire.info(f"EVENT: {event}")
         logfire.info(f"ID: {approval_id}")
-        # logfire.info(f"MESSAGE: {approval_text}")
-        async with TaskParser() as parser:
-            task_info =  await parser.get_simplified_approval(approval_id)
-        logfire.info(f"{task_info}")
+        if "полностью согласован" in approval_text.lower():
+            url_task = f"https://akv.task-tm.com:30201/tm/approval/show/{approval_id}"
+            
+            async with TaskParser() as parser:
+                task_info =  await parser.get_simplified_approval(approval_id)
+
+            await tehzak_email.send_email(
+                    html_table=f"""
+            <html>
+            <head>
+                <style>
+                    th {{
+                        background-color: #005C8C;
+                        color: #ffffff;
+                    }}
+                        th, td {{
+                        font-size: 12px;
+                        padding: 2px 10px;
+                    }}
+                </style>
+            </head>
+            <body>
+                <p><a href="{url_task}/">{url_task}</a>'</p>
+                <h1>{approval_text}<h2>
+                <table border="0">
+                    <tr>
+                        <th>Ответсвенный</th>
+                        <th>Согласовано?</th>
+                        <th>Дата согласования</th>
+                    </tr>
+                    {generate_approvers_html(task_info.get('approvers'))}
+                </table>
+                <p></p>
+                <p style='font-size: 12px; color: grey;' >Это письмо сформировано автоматически. Отвечать на него не нужно.</p>
+            </body>
+            </html>
+            """,
+                    email_subj=f"{approval_id} | Согласован | {process_string(task_info.get('subj')), 30}",
+                    email_list=["gricyuk@group-akvilon.ru"]
+                )
+        logfire.info(f"{approval_text}")
         return
 
     if event == "event":
